a04fc8d2e34a0cdbcf7893994c6fdba1ab063994
Ife Adeyefa <iadeyefa@ufl.edu>
2025-12-18 22:05:32 -0500

    Create tabs and Bio tab content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import linkedinLogo from './assets/linkedin.png'
-import githubLogo from './assets/github.svg'
 import './App.css'
+import Tabs from './components/Tabs'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://github.com/iadeyefa" target="_blank">
-          <img src={githubLogo} className="logo" alt="GitHub logo" />
-        </a>
-        <a href="https://www.linkedin.com/in/ife-adeyefa-9b7577300/" target="_blank">
-          <img src={linkedinLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Ife Adeyefa</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="App">
+                <Tabs />
+            </div>
+        </>
+    )
 }
 
 export default App
