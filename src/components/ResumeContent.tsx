90ea7bab304f31e3b4ce1e6d2c3dfc6fcc07bf0d
Ife Adeyefa <iadeyefa@ufl.edu>
2025-12-20 20:34:28 -0500

    Update ResumeContent.tsx

--- src/components/ResumeContent.tsx
+++ src/components/ResumeContent.tsx
@@ -8,16 +8,12 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 export default function ResumeContent() {
     return (
         <div className="tab-content">
-            <iframe
-                src="/resume.pdf"
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    border: 'none',
-                    borderRadius: '8px'
-                }}
-                title="Resume"
-            />
+            <object
+                data="/resume.pdf"
+                type="application/pdf"
+                width="100%"
+                height="800px">
+            </object>
         </div>
     )
 }
